37c43909053ae52e6b75943f22cee3f002a3275a
ganeshpaiya <ganesanhere4u@gmail.com>
2024-08-29 05:50:16 +0530

    Add project files.

---
+++ Task.API/Controllers/TasksController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task.API.Models;
+
+namespace Task.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        //CREATE =>POST
+        //READ   =>GET
+        //UPDATE =>PUT/PATCH
+        //DELETE =>DELETE
+
+        //in memory  store
+
+         public static readonly List<TodoItem> _todoItems=[];
+        //GET api/tasks
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TodoItem>> Get()
+        {
+            return Ok(_todoItems);
+        }
+
+        //GET api/task/1
+
+        [HttpGet("{id}")]
+         public ActionResult<TodoItem> Get(int id)
+         {
+           var todoItem = _todoItems.FirstOrDefault(x => x.Id == id);
+            if (todoItem != null)
+            {
+                return Ok(todoItem);
+            }
+            return Ok(todoItem);
+         }
+
+        //POST api/tasks
+
+
+
+       
+
+        
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+    }
+}
